efe71e4be9662b79ff27c9de05ddb4ee9ba7bd59
Henrique Almeida <59674959+hicones@users.noreply.github.com>
2025-01-22 15:22:02 -0300

    feat: home page

---
+++ src/app/(app)/components/navigation.tsx
@@ -0,0 +1,36 @@
+import { TbClock, TbSend, TbSquareRoundedArrowDown } from "react-icons/tb";
+
+const navigationItems = [
+  {
+    icon: <TbSend size={28} />,
+    label: "Pagar",
+  },
+  {
+    icon: <TbSquareRoundedArrowDown size={28} />,
+    label: "Cobrar",
+  },
+  {
+    icon: <TbClock size={28} />,
+    label: "Histórico",
+  },
+];
+
+export const Navigation = () => {
+  return (
+    <div className="flex w-full items-center justify-center px-2">
+      <ul className="flex gap-6 items-center text-text-primary w-full justify-center">
+        {navigationItems.map((item, index) => (
+          <li
+            key={index}
+            className="flex flex-col items-center justify-center gap-2 font-medium first:hover:font-semibold transition-all duration-300"
+          >
+            <button className="flex items-center justify-center rounded-full border border-text-primary p-2 hover:scale-105 transition-all">
+              {item.icon}
+            </button>
+            {item.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
---
+++ src/components/app/bottom-navigation.tsx
@@ -0,0 +1,35 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { TbHome, TbUsersGroup, TbUsers, TbUser } from "react-icons/tb";
+
+const bottomNavigationLinks = [
+  { label: "Home", icon: TbHome, href: "/" },
+  { label: "Grupos", icon: TbUsersGroup, href: "/groups" },
+  { label: "Contatos", icon: TbUsers, href: "/contact" },
+  { label: "Conta", icon: TbUser, href: "/account" },
+];
+
+export const BottomNavigation = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex items-center justify-around w-full p-4 mt-auto">
+      {bottomNavigationLinks.map((link) => (
+        <Link
+          key={link.href}
+          href={link.href}
+          className={cn(
+            "flex flex-col items-center justify-center",
+            pathname === link.href
+              ? "font-semibold text-text-primary"
+              : "text-text-secondary font-light",
+          )}
+        >
+          <link.icon size={24} />
+          <span className="text-xs">{link.label}</span>
+        </Link>
+      ))}
+    </nav>
+  );
+};
