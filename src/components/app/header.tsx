5f7a361d7338a701e9ca1c958f8d1c4b31e54afd
Henrique Almeida <59674959+hicones@users.noreply.github.com>
2025-01-23 14:06:49 -0300

    feat: add notification ball

--- src/components/app/header.tsx
+++ src/components/app/header.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { IoNotificationsOutline } from "react-icons/io5";
 
 export const Header = () => {
+  const hasNotification = true;
+
   return (
     <nav className="w-full flex items-center justify-between gap-4">
       <Image
@@ -13,7 +15,12 @@ export const Header = () => {
 
       <h2 className="font-semibold text-lg text-text-primary">Splitify</h2>
 
-      <IoNotificationsOutline className="size-6 text-text-primary cursor-pointer hover:scale-105 transition-all hover:rotate-12" />
+      <div className="flex p-1 group">
+        <IoNotificationsOutline className="size-6 text-text-primary cursor-pointer hover:scale-105 transition-all hover:rotate-12" />
+        {hasNotification && (
+          <div className="w-2 h-2 bg-red-500 rounded-full group-hover:scale-110 transition-all"></div>
+        )}
+      </div>
     </nav>
   );
 };
